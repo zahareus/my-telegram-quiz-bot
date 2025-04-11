6defb28333bfb3b356d8431ef5241599e951614f
Ksenobite <beta.ksenobite@mail.ru>
2025-04-11 05:18:44 +0300

    add scrap.sh

--- scrap.sh
+++ scrap.sh
@@ -1,2 +1,2 @@
-docker exec summarize_bot-summarize_bot-1 sh -c "/usr/src/summarize_bot && /usr/src/summarize_bot/.venv/bin/python3 /usr/src/summarize_bot/scrap.py"
+docker exec summarize_bot-summarize_bot-1 sh -c "cd /usr/src/summarize_bot && /usr/src/summarize_bot/.venv/bin/python3 /usr/src/summarize_bot/scrap.py"
 echo "Scrap started: $(date)"
